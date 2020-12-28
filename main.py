ef255920915e7de506551ad200b36356289eace6
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2020-12-28 10:35:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import string
+import pyinstaller
+
+def get_random_password():
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    password = random.choice(string.ascii_lowercase)
+    password += random.choice(string.ascii_uppercase)
+    password += random.choice(string.digits)
+    password += random.choice(string.punctuation)
+
+    for i in range(15):
+        password += random.choice(random_source)
+
+    password_list = list(password)
+    random.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
+    return password
+
+print("First Random Password is ", get_random_password())
+print("Second Random Password is ", get_random_password())
